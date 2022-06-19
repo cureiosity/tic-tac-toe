31b286f07f4f319264d9e905c1e3c6d1f96d2c80
Adam Bachrach <nalc1100adam@gmail.com>
2022-06-18 23:39:37 -0400

    alternate between o's and x's when clicking on squares

--- tictactoe.js
+++ tictactoe.js
@@ -10,8 +10,23 @@ const tttSquares = document.querySelectorAll(".ttt-square")
 tttSquares.forEach((square) => {
   square.addEventListener("mouseover", toggleHighlight)
   square.addEventListener("mouseout", toggleHighlight)
+  square.addEventListener("click", markSquare)
 })
 
 function toggleHighlight(e) {
   e.target.classList.toggle("active")
+}
+
+let clickCount = 0
+function markSquare(e) {
+  clickCount++
+  let filler = null
+  if (clickCount % 2 === 0) {
+    filler = "O"
+  } else {
+    filler = "X"
+  }
+  const playerMark = document.createElement("h1")
+  playerMark.innerText = filler
+  e.target.appendChild(playerMark)
 }
