a8c210bf842ac2431ebca596ec1f0ce8a15509b3
Adam Bachrach <nalc1100adam@gmail.com>
2022-06-20 10:45:04 -0400

    add a class for unclicked squares

--- tictactoe.js
+++ tictactoe.js
@@ -2,6 +2,7 @@ const main = document.querySelector("main")
 for (i = 0; i < 9; i++) {
   const tttDiv = document.createElement("div")
   tttDiv.classList.add("ttt-square")
+  tttDiv.classList.add("unclicked")
   tttDiv.id = `box${i + 1}`
   main.appendChild(tttDiv)
 }
@@ -19,11 +20,15 @@ function toggleHighlight(e) {
 
 let clickCount = 0
 function markSquare(e) {
-  clickCount++
-  if (clickCount % 2 !== 0) {
-    e.target.classList.add("player-red")
-  } else {
-    e.target.classList.add("player-blue")
+  if (e.target.classList.contains("unclicked")) {
+    clickCount++
+    console.log(clickCount)
+    e.target.classList.remove("unclicked")
+    if (clickCount % 2 !== 0) {
+      e.target.classList.add("player-red")
+    } else {
+      e.target.classList.add("player-blue")
+    }
   }
 }
 
@@ -31,8 +36,11 @@ const resetButton = document.querySelector("#reset")
 console.log(resetButton.innerText)
 resetButton.addEventListener("click", resetBoard)
 function resetBoard() {
+  clickCount = 0
+  console.log(clickCount)
   tttSquares.forEach((square) => {
     square.classList.remove("player-red")
     square.classList.remove("player-blue")
+    square.classList.add("unclicked")
   })
 }
